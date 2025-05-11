e50e73bef13c4ea5463e027835eb9dc83ecc0b35
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2025-05-11 01:56:29 +0000

    Deploying to gh-pages from @ tatami-inc/tatami_stats@dce3afdbc04ea33646678e5edd21da8585dabbc0 🚀

--- docs/search/files_f.js
+++ docs/search/files_f.js
@@ -1,4 +1,6 @@
 var searchData=
 [
-  ['utils_2ehpp_0',['utils.hpp',['../utils_8hpp.html',1,'']]]
+  ['tatami_2ehpp_0',['tatami.hpp',['https://tatami-inc.github.io/tatami/tatami_8hpp.html',1,'']]],
+  ['tatami_5fstats_2ehpp_1',['tatami_stats.hpp',['../tatami__stats_8hpp.html',1,'']]],
+  ['transpose_2ehpp_2',['transpose.hpp',['https://tatami-inc.github.io/tatami/transpose_8hpp.html',1,'']]]
 ];
--- docs/search/variables_3.js
+++ docs/search/variables_3.js
@@ -1,7 +1,4 @@
 var searchData=
 [
-  ['skip_5fnan_0',['skip_nan',['../structtatami__stats_1_1grouped__medians_1_1Options.html#aaefc94bcd05df52b217ff11c976e124b',1,'tatami_stats::grouped_medians::Options::skip_nan'],['../structtatami__stats_1_1grouped__sums_1_1Options.html#a0eefb6cb47a631d15c577f54fbb47941',1,'tatami_stats::grouped_sums::Options::skip_nan'],['../structtatami__stats_1_1grouped__variances_1_1Options.html#a00d2ca136b913d98aef0e918c49f07c8',1,'tatami_stats::grouped_variances::Options::skip_nan'],['../structtatami__stats_1_1medians_1_1Options.html#a2fa7e058d6053d7f593554ce67a34145',1,'tatami_stats::medians::Options::skip_nan'],['../structtatami__stats_1_1ranges_1_1Options.html#a15b5deec811e94c441d714abdad16692',1,'tatami_stats::ranges::Options::skip_nan'],['../structtatami__stats_1_1sums_1_1Options.html#a9fade20069b7314f5a43315b66a08e7d',1,'tatami_stats::sums::Options::skip_nan'],['../structtatami__stats_1_1variances_1_1Options.html#aeb3ee96b5c18332bd57e0633c3593c53',1,'tatami_stats::variances::Options::skip_nan']]],
-  ['sparse_5fextract_5findex_1',['sparse_extract_index',['https://tatami-inc.github.io/tatami/structtatami_1_1Options.html#a192a47606233c7f5c76bb1e7a62867a5',1,'tatami::Options']]],
-  ['sparse_5fextract_5fvalue_2',['sparse_extract_value',['https://tatami-inc.github.io/tatami/structtatami_1_1Options.html#abe1ec7b8bfc82728742ca4a496e59548',1,'tatami::Options']]],
-  ['sparse_5fordered_5findex_3',['sparse_ordered_index',['https://tatami-inc.github.io/tatami/structtatami_1_1Options.html#a4d2e6f2631a7e813bfbf3f215594a79b',1,'tatami::Options']]]
+  ['pointers_0',['pointers',['https://tatami-inc.github.io/tatami/structtatami_1_1CompressedSparseContents.html#ad9410e8d3b32bc97086bebf2322c0ef8',1,'tatami::CompressedSparseContents']]]
 ];
