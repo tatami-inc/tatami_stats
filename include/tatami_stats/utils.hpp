c7bb31837be4a605ac9a7cd028a749ed92fef629
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2026-02-02 11:57:02 +1100

    Fixed I<> to be consistent with C++20's remove_cvref_t.

--- include/tatami_stats/utils.hpp
+++ include/tatami_stats/utils.hpp
@@ -21,7 +21,7 @@ namespace tatami_stats {
  * @cond
  */
 template<typename Input_>
-using I = std::remove_reference_t<std::remove_cv_t<Input_> >;
+using I = std::remove_cv_t<std::remove_reference_t<Input_> >;
 /**
  * @endcond
  */
